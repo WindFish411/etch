6fbdc76eb7aa6b9f724f7183e9d9ea5fb80d413a
Tyler Strunk <wtstrunk1@gmail.com>
2024-01-19 14:17:45 -0500

    I have stored the box.div in a 2d array, but the loops are not functioning properly. Committing now in case I have to do a major revamp.

--- etch.js
+++ etch.js
@@ -4,18 +4,32 @@
 
 //const gridNumber = document.getElementById("grid");
 const button1 = document.getElementById("button");
-let boxNumber = 0; 
 
 button1.addEventListener('click',() => createGrid(16));
 
-
 function createGrid(boxNumber){
-    const grid = [];
+    //need to create box divs inside of function. I'll worry about activating the input/button later. 
+    //First step is to get the 16x16 grid displayed on the page. 
+
+    const container = document.getElementById("container");
+    container.innerHTML = "";
+    let grid = [];
+
     for (let i = 0; i < boxNumber; i++) {
         grid[i] = [];
         for (let j = 0; j < boxNumber; j++) {
-            grid[i][j] = 0; 
-           console.log(grid);
-        }
+            const box = document.createElement('div');
+            box.className = 'box';
+            //container.appendChild(box);
+            grid[i][j] = container.appendChild(box);
+            container.appendChild(grid[i][j]);
+            console.log(i);
+            console.log(j);
+      }
     }
 }
+
+
+
+
+
