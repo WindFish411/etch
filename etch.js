e49e76dd2ad42b48ae2abfdccba193dbb2e9b33c
Tyler Strunk <wtstrunk1@gmail.com>
2024-01-19 15:16:55 -0500

    I think I am closer to the solution now just using one simple loop and leveraging flex boxes. I can't get the lowerright corner of the box to fill, so now quite there yet.

--- etch.js
+++ etch.js
@@ -2,21 +2,38 @@
 
 
 
-//const gridNumber = document.getElementById("grid");
+
 const button1 = document.getElementById("button");
-const button2 = document.getElementById("button2");
-
 button1.addEventListener('click',() => createGrid(16));
-button2.addEventListener('click',() => displayGrid())
 
-function createGrid(boxNumber){
 
+function createGrid(boxNumber) {
     const container = document.getElementById("container");
-    
+  
+    for (let i = 0; i < boxNumber; i++) {
+      const box = document.createElement('div');
+      box.className = 'box';
+      container.appendChild(box);
+    }
+  }
+  
+ 
 
 
+
+
+
+
+
+
+//Rebuilding logic. Saving old logic just in case. 
+
+//button2.addEventListener('click',() => displayGrid())
+
+/*
+function createGrid(boxNumber){
+    const container = document.getElementById("container");
     let grid = [];
-
     for (let i = 0; i < boxNumber; i++) {
         grid[i] = [];
         for (let j = 0; j < boxNumber; j++) {
@@ -29,7 +46,6 @@ function createGrid(boxNumber){
     return grid;
 }
 
-
 const container = document.getElementById("container");
 
 function displayGrid(grid){
@@ -41,7 +57,7 @@ function displayGrid(grid){
         }
     }
 }
-
+*/
 
 
 
