9743a98c47784e8ca90ce28edc5a38eaea41f13a
Tyler Strunk <wtstrunk1@gmail.com>
2024-01-19 18:07:34 -0500

    The current version is working well. You have to type a number into the box and press the button. Still need to 1.) Initialize a grid upon opening the page. 2.)Add a button that prompts the user for input instead of just having a blank box. 3.)Make sure values are betwee 1 & 100.

--- etch.js
+++ etch.js
@@ -1,4 +1,36 @@
 
+const button1 = document.getElementById("button");
+button1.addEventListener('click', function(){
+    let gridInput = document.getElementById("grid").value;
+    let size = gridInput;
+    createGrid(size);
+});
+
+function createGrid(size) {
+    const container = document.getElementById('container');
+    container.innerHTML = '';
+
+    
+
+    container.style.setProperty('--columns', size);
+    container.style.setProperty('--rows', size);
+
+    for (let i = 0; i < (size * size); i++) {
+      const box = document.createElement('div');
+      box.className = 'box';
+      container.appendChild(box);
+
+      box.addEventListener('mouseover', function(){
+            const randomColor = randomRgbColor();
+            box.style.backgroundColor = `rgb(${randomColor[0]}, ${randomColor[1]}, ${randomColor[2]}`
+    
+      });
+
+    }
+  }
+  
+
+
 //randomInteger + randomRgbColor generates a random color. 
 function randomInteger(max) {
     return Math.floor(Math.random()*(max + 1));
@@ -13,35 +45,6 @@ function randomRgbColor() {
     //need to find a way to add this to CSS for boxes
 }
 
-
-const button1 = document.getElementById("button");
-button1.addEventListener('click', function(){
-    let gridInput = document.getElementById("grid").value;
-    let size = gridInput;
-    createGrid(size);
-});
-
-function createGrid(size) {
-    const gridInput = document.getElementById('container');
-    container.innerHTML = '';
-
-    container.style.setProperty('--columns', size);
-    container.style.setProperty('--rows', size);
-
-    for (let i = 0; i < (size * size); i++) {
-      const box = document.createElement('div');
-      box.className = 'box';
-      container.appendChild(box);
-
-      box.addEventListener('mouseover', function(){
-            const randomColor = randomRgbColor();
-            box.id = 'coloredBox';
-            coloredBox.style.backgroundColor = `rgb(${randomColor[0]}, ${randomColor[1]}, ${randomColor[2]}}`
-      });
-    }
-  }
-  
-//How I want to approach: Create new class -> upon mouse hover -> add new class
 
 
 
