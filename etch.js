d169b45bc4275406351ed2333b635be839a86a10
Tyler Strunk <wtstrunk1@gmail.com>
2024-01-19 12:13:38 -0500

    Haven't made much pract. Just realized I haven't created a single flex box. This marks where I start implementing flex boxes to create my grid.

--- etch.js
+++ etch.js
@@ -1,19 +1,21 @@
-//Need to use Javascript to create a 16x16 grid. 
-
-// Creating a 16x16 grid (2D array)
 
 
 
-//prompt user for # of squares for grid
-//add event listener upon entering input to run function createGrid(numberOfSquares)
-const square = document.getElementByClass("square");
 
-function createGrid(){
+//const gridNumber = document.getElementById("grid");
+const button1 = document.getElementById("button");
+let boxNumber = 0; 
+
+button1.addEventListener('click',() => createGrid(16));
+
+
+function createGrid(boxNumber){
     const grid = [];
-    for (let i = 0; i < 16; i++) {
+    for (let i = 0; i < boxNumber; i++) {
         grid[i] = [];
-        for (let j = 0; j < 16; j++) {
-            grid[i][j] = 0; // Initialize each cell with a value (e.g., 0)
+        for (let j = 0; j < boxNumber; j++) {
+            grid[i][j] = 0; 
+           console.log(grid);
         }
     }
 }
